1a6751c97f3bd717b8cd0f51abf887f7f6e4adb5
thuocsi.kietcht <kiet.chung@thuocsi.vn>
2022-12-12 22:04:23 +0700

    first-push

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
+	"os"
 
 	"kietchung/controllers"
 	"kietchung/services"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -25,10 +28,24 @@ var (
 	err          error
 )
 
+const (
+	USERNAME = "username"
+	PASSWORD = "password"
+)
+
 func init() {
 	ctx = context.TODO()
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	mongoconn := options.Client().ApplyURI("mongodb+srv://tuankiet10022171:kietlu1712@cluster0.znigccy.mongodb.net/?retryWrites=true&w=majority")
+	username := os.Getenv(USERNAME)
+	password := os.Getenv(PASSWORD)
+	//mongoconn := options.Client().ApplyURI("mongodb+srv://tuankiet10022171:kietlu1712@cluster0.znigccy.mongodb.net/?retryWrites=true&w=majority")
+	mongoConn := fmt.Sprintf("mongodb://%s:%s@localhost:27017/chemistry", username, password)
+	mongoconn := options.Client().ApplyURI(mongoConn)
 	mongoclient, err = mongo.Connect(ctx, mongoconn)
 	if err != nil {
 		log.Fatal("error while connecting with mongo", err)
